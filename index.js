9715ed86953ec41d70aa7615b3cfc48335cbb323
minabadrous <minabadrous.vu@gmail.com>
2020-07-23 19:46:42 +0200

    enhanced quotations

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ var quotes = ['The ocean is made of drops.',
     'Hell is made for people like you.',
     'Live as if you were to die tomorrow. Learn as if you were to live forever.',
     'That which does not kill us makes us stronger.',
-    'Be who you are and say what you feel, because those who mind don’t matter and those who matter don’t mind.',
-    'We must not allow other people’s limited perceptions to define us.',
+    "Be who you are and say what you feel, because those who mind don't matter and those who matter don't mind.",
+    "We must not allow other people's limited perceptions to define us.",
     'Do what you can, with what you have, where you are.',
     'Be yourself; everyone else is already taken.',
     'This above all: to thine own self be true.',
     'If you cannot do great things, do small things in a great way.',
-    'If opportunity doesn’t knock, build a door.'
+    "If opportunity doesn't knock, build a door."
   ];
 var authors = ['Moana',
 'Whiplash',
